8d87e67125965be9c04a0ce359aea2cddd03e529
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-12-16 12:19:44 +0500

    format_kline_data

--- app/http/utils/format_kline_data.py
+++ app/http/utils/format_kline_data.py
@@ -9,13 +9,13 @@ def format_kline_data(data: KlineHttpResult):
     for elem in list:
         formatted_list.append(
             {
-                "startTime": datetime.fromtimestamp(int(elem[0]) / 1000).strftime(
+                "time": datetime.fromtimestamp(int(elem[0]) / 1000).strftime(
                     "%d-%m-%Y %H:%M:%S"
                 ),
-                "openPrice": float(elem[1]),
-                "highPrice": float(elem[2]),
-                "lowPrice": float(elem[3]),
-                "closePrice": float(elem[4]),
+                "open": float(elem[1]),
+                "high": float(elem[2]),
+                "low": float(elem[3]),
+                "close": float(elem[4]),
                 "volume": float(elem[5]),
                 "turnover": float(elem[6]),
             }
