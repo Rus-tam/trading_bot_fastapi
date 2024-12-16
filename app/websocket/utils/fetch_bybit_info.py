b7317e30dffc6a858d2a5552596aba744e52b1db
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-12-16 13:03:43 +0500

    getting last 1000 kline data points

--- app/websocket/utils/fetch_bybit_info.py
+++ app/websocket/utils/fetch_bybit_info.py
@@ -4,6 +4,8 @@ from ..schemas import klineData, klineResponse
 from ...computer.chaikin_osc import chaikin_osc
 import pandas as pd
 from .format_kline_ws_data import format_kline_ws_data
+from ...http.utils.fetch_bybit_data import fetch_bybit_data
+from ...http.utils.format_kline_data import format_kline_data
 
 
 async def fetch_bybit_info(symbol: str, interval: str):
@@ -11,6 +13,10 @@ async def fetch_bybit_info(symbol: str, interval: str):
     subscription_params = {"op": "subscribe", "args": [f"kline.{interval}.{symbol}"]}
 
     confirmed_data = []
+    first_timestamp = 0
+    start_time = 0
+    end_time = 0
+    kline_data_historical = []
 
     try:
         async with websockets.connect(url) as ws:
@@ -26,10 +32,28 @@ async def fetch_bybit_info(symbol: str, interval: str):
                 if "data" in klineInfo:
                     data: klineData = klineInfo["data"][0]
 
-                    formatted_data = format_kline_ws_data(data)
+                    if data["confirm"]:
+                        formatted_data = format_kline_ws_data(data)
 
-                    print(formatted_data)
-                    print(" ")
+                        # Вычисления временных отрезков для произведения http запроса
+                        if first_timestamp == 0:
+                            first_timestamp = data["end"]
+                            start_time = first_timestamp - 1000 * 60000
+                            end_time = first_timestamp - 60000
+
+                            endpoint = "/v5/market/kline"
+                            params = {
+                                "category": "spot",
+                                "symbol": symbol,
+                                "interval": interval,
+                                "start": start_time,
+                                "end": end_time,
+                                "limit": 1000,
+                            }
+
+                            historical_kline = await fetch_bybit_data(endpoint, params)
+
+                            print(historical_kline["result"]["list"])
 
                     # if data["confirm"]:
                     #     confirmed_data.append(data)
